18182c62a45e0066fc9e89089bcdba30236ece6f
ro-mak <eee127@yandex.ru>
2021-07-07 07:26:30 +0300

    removed lists/arrays

--- lesson2/task2-2.py
+++ lesson2/task2-2.py
@@ -1,12 +1,13 @@
 # Найти сумму n элементов следующего ряда чисел: 1, -0.5, 0.25, -0.125,…
 # Количество элементов(n) вводится с клавиатуры.
 n = int(input("Input number of elements to sum up: "))
-progression = [1, -0.5, 0.25, -0.125]
-q = progression[1] / progression[0]
+progression_first = 1
+progression_second = -0.5
+q = progression_second / progression_first
 i = 1
-sum = progression[0]
+sum = progression_first
 while i < n:
-    next = progression[0] * (q ** i)
+    next = progression_first * (q ** i)
     sum += next
     i += 1
 print(f"Sum is {sum}")
