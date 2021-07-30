383049e50766c9866b6a713f37b7e070c4f67189
ro-mak <eee127@yandex.ru>
2021-07-30 20:06:19 +0300

    made everything "readable" and used named tuple

--- lesson5/task1.py
+++ lesson5/task1.py
@@ -6,13 +6,12 @@ names = l[::2]
 profits = l[1::2]
 
 Company = namedtuple("Company", "company_name, profit")
-company_dict = dict(zip(names, map(int, profits)))
-
-total_profit = int(sum(company_dict.values()))
+company_list = list(map(lambda kv: Company(kv[0], kv[1]), dict(zip(names, map(int, profits))).items()))
+total_profit = int(sum(list(map(lambda c: c.profit, company_list))))
 average_profit = total_profit // n
 
 print(f"Average profit {average_profit}")
-for name, profit in company_dict.items():
+for name, profit in company_list:
     if profit > average_profit:
         print(f"Company {name}'s profit is bigger than average")
     elif profit < average_profit:
