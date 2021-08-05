85be69c27230cb63d9c98055ce656a66b2152596
ro-mak <eee127@yandex.ru>
2021-08-05 21:44:12 +0300

    added analysis result

--- lesson6/task1.py
+++ lesson6/task1.py
@@ -2,6 +2,15 @@ import sys
 # В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
 from random import randint
 
+print()
+print()
+print()
+print(f"Python: {sys.version}")
+print(f"Platform: {sys.platform}")
+print()
+print()
+print()
+
 
 def show_size(x, level=0):
     print('\t' * level, f'type= {x.__class__}, size= {sys.getsizeof(x)}, object= {x}')
@@ -43,3 +52,22 @@ show_size(max)
 show_size(max_index)
 print(
     f"Total size = {sys.getsizeof(random_numbers) + sys.getsizeof(min) + sys.getsizeof(min_index) + sys.getsizeof(max) + sys.getsizeof(max_index)}")
+
+# Python: 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)]
+# Platform: win32
+# Total size = 292
+# type= <class 'list'>, size= 184, object= [93, 541, 941, 560, 444, 688, 952, 725, 841, 376]
+#  type= <class 'int'>, size= 28, object= 93
+#  type= <class 'int'>, size= 28, object= 541
+#  type= <class 'int'>, size= 28, object= 941
+#  type= <class 'int'>, size= 28, object= 560
+#  type= <class 'int'>, size= 28, object= 444
+#  type= <class 'int'>, size= 28, object= 688
+#  type= <class 'int'>, size= 28, object= 952
+#  type= <class 'int'>, size= 28, object= 725
+#  type= <class 'int'>, size= 28, object= 841
+#  type= <class 'int'>, size= 28, object= 376
+# type= <class 'int'>, size= 28, object= 93
+# type= <class 'int'>, size= 28, object= 6
+# type= <class 'int'>, size= 28, object= 952
+# type= <class 'int'>, size= 24, object= 0
