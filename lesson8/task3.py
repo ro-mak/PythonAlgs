f9cf3b28ec7b624f8712ac4f3cc00a63f344f826
ro-mak <eee127@yandex.ru>
2021-09-05 23:19:41 +0300

    implemented deep search

--- lesson8/task3.py
+++ lesson8/task3.py
@@ -4,12 +4,18 @@
 # a. граф должен храниться в виде списка смежности;
 # b. генерация графа выполняется в отдельной функции, которая принимает на вход число вершин.
 from random import randint
+from collections import deque
 
 
 def generate_graph(n):
     g = []
     for i in range(0, n):
-        g.append([randint(0, n) for j in range(0, n)])
+        l = [randint(0, 1) for j in range(0, n)]
+        if 0 not in l:
+            l[n - 1] = 0
+        if 1 not in l:
+            l[n - 1] = 1
+        g.append(l)
     for i in range(len(g)):
         print(g[i])
     return g
@@ -20,4 +26,23 @@ gr = generate_graph(size)
 
 
 def deep_search(graph, start):
-    pass
+    visited_nodes = deque()
+    nodes_to_visit = deque()
+    next_node = start
+    while len(visited_nodes) < len(graph):
+        visited_nodes.append(next_node)
+        child_nodes = graph[next_node]
+        for index, node in enumerate(child_nodes):
+            if index == next_node:
+                continue
+            if index not in visited_nodes and node == 1:
+                nodes_to_visit.append(index)
+        if len(nodes_to_visit) > 0:
+            next_node = nodes_to_visit.pop()
+        else:
+            break
+    return visited_nodes
+
+
+s = int(input("Start: "))
+print(deep_search(gr, s))
